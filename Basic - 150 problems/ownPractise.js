181125c60462653492ef33bf8a50994d4adfcb1c
Muhtasim Rabib <muhtasimrabib2@gmail.com>
2026-07-15 18:30:09 +0600

    practise findIndex() method

--- Basic - 150 problems/ownPractise.js
+++ Basic - 150 problems/ownPractise.js
@@ -1,3 +1,6 @@
+/**
+
+// Practising this 
 // console.log(`Hello Idiot!`);
 console.log(`To find the largest element in an array`);
 const arr = [21, 23, 4, 51, 12, 63, 21, 22, 32, 12, 32, 55];
@@ -58,4 +61,85 @@ rayen.printPlayerName();
 console.log("");
 console.log("");
 
-console.log(rabib, rayen);
+console.log(rabib, rayen);1
+
+*/ const init = function () {
+  // practising question answers with findIndex()
+
+  // easy Use findIndex() to find the index of "Orange".
+  const fruits = ["Apple", "Banana", "Orange", "Mango"];
+  const fruits2 = ["Apple", "Banana", "Mango"];
+
+  // const fruitFinderIndex = function (array) {
+  //   return array.findIndex((element) => {
+  //     return element === "Orange";
+  //   });
+  // };
+  const fruitFinderIndex = function (array) {
+    return array.findIndex((element) => element === "Orange");
+  };
+
+  console.log(fruitFinderIndex(fruits));
+  console.log(fruitFinderIndex(fruits2));
+
+  // mediutm - find the index of the element whose items name is bread
+
+  const groceryItems = [
+    { name: "Eggs", quantity: 4, completedStatus: false },
+    { name: "Bread", quantity: 2, completedStatus: true },
+    { name: "Butter", quantity: 1, completedStatus: false },
+    { name: "Milk", quantity: 1, completedStatus: false },
+  ];
+
+  // console.log(groceryItems[0].name);
+  // console.log(groceryItems[1].name);
+
+  // console.log(groceryItems.forEach((e, i, a) => console.log(a[i])));
+
+  console.log(
+    groceryItems.findIndex((element, idx, arr) => element.name === "Bread"),
+  );
+
+  // hard
+  // if butter exists set the completedStatus from false to true
+  console.log("");
+  console.log("");
+
+  groceryItems.findIndex((element) => {
+    if (element.name === "Butter") {
+      return (element.completedStatus = true);
+    }
+  });
+
+  console.log(groceryItems);
+  console.log("");
+  console.log("");
+
+  const groceryItems22 = [
+    { name: "Eggs", quantity: 4, completedStatus: false },
+    { name: "Bread", quantity: 2, completedStatus: true },
+    { name: "Banana", quantity: 2, completedStatus: true },
+    { name: "Butter", quantity: 1, completedStatus: false },
+    { name: "Milk", quantity: 1, completedStatus: false },
+  ];
+
+  const removeItem = function (array, itemToBeRemoved) {
+    console.log(array, itemToBeRemoved);
+
+    const indexToBeRemoved = array.findIndex((element) => {
+      return element.name === itemToBeRemoved;
+    });
+    console.log("");
+    console.log(indexToBeRemoved);
+
+    const removeElement = array.splice(indexToBeRemoved, 1);
+
+    console.log(array);
+  };
+
+  removeItem(groceryItems22, "Butter");
+
+  // ends here
+};
+
+init();
